427642972ad54896d2b8840b1b8e24cda9d49d74
ashish-singh-bist <ashish.singh.bist@gmail.com>
2018-10-02 10:41:02 +0530

    add stats script

---
+++ parsing_scripts/create_stats_booking.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+import os
+import time
+import sys, getopt
+#import csv
+#import requests
+import re
+#import hashlib
+import json
+import datetime
+import hashlib
+from datetime import timedelta, date
+ #import threading
+
+#####################
+processoutput = os.popen("ps -A -L -F").read()
+cur_script = os.path.basename(__file__)
+res = re.findall(cur_script,processoutput)
+if len(res)>2:
+    print ("EXITING BECAUSE ALREADY RUNNING.\n\n")
+    exit(0)
+#####################
+
+sys.path.append("modules")
+sys.path.append("scripts")
+sys.path.append("/usr/local/lib/python3.5/dist-packages")
+from Master import Master
+from Booking import Booking
+obj_master = Master()
+html_dir_path = obj_master.obj_config.html_dir_path
+
+def getDateTimeObject(date_str):
+  datetime_object = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+  return datetime_object
+
+def getLastDateObject():
+  current_date = datetime.datetime.now().date()
+  last_date = current_date - timedelta(days=1)  # decrease day one by one
+  last_date_object = getDateTimeObject(str(last_date))
+  return last_date_object  
+
+
+if __name__ == '__main__':
+  dict_stats = {}
+
+  config_rows = obj_master.obj_mongo_db.recSelect('config')
+  for config_row in config_rows:    
+    if 'parsing_interval' in config_row and config_row['parsing_interval']:
+      parsing_interval = int(config_row['parsing_interval'])      
+      date_before_time_interval = datetime.datetime.now() - timedelta(days=parsing_interval)
+      dict_stats['property_pending'] = obj_master.obj_mongo_db.getCount('property_urls',None,{'is_active':1,'updated_at':{ '$lt': date_before_time_interval }})
+    
+  last_date_obj = getLastDateObject()
+  current_date_obj = getDateTimeObject(str(datetime.datetime.now().date()))
+
+  dict_stats['date'] = last_date_obj
+  dict_where = { 'updated_at':{ '$gte': last_date_obj , '$lt': current_date_obj } }
+  dict_stats['property_parsed'] = obj_master.obj_mongo_db.getCount('property_urls',None,dict_where)
+  dict_stats['hotel_parsed'] = obj_master.obj_mongo_db.getCount('hotel_master',None,dict_where)
+  dict_stats['price_parsed'] = obj_master.obj_mongo_db.getCount('hotel_prices',None,dict_where)
+  dict_stats['room_details_parsed'] = obj_master.obj_mongo_db.getCount('room_details',None,dict_where)
+  dict_stats['rooms_availability_parsed'] = obj_master.obj_mongo_db.getCount('rooms_availability',None,dict_where)
+  
+  dict_stats['total_property'] = obj_master.obj_mongo_db.getCount('property_urls')
+  dict_stats['active_property'] = obj_master.obj_mongo_db.getCount('property_urls',None,{'is_active':1})
+  print(str(dict_stats))
+  ret_id = obj_master.obj_mongo_db.recInsert( 'stats_booking' , [ dict_stats ] )
+  print( "\ninserted in stats table. The return id is"+str(ret_id) )
+
+    
